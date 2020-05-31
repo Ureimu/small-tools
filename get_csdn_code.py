7d33946d327a10f072be6c255eaebde6fa77c688
Ureimu <57444811+Ureimu@users.noreply.github.com>
2020-05-31 18:42:04 +0800

    Update get_csdn_code.py

--- get_csdn_code.py
+++ get_csdn_code.py
@@ -2,6 +2,7 @@ import re
 
 """
 将csdn的代码块的源代码输入,可以得到代码块的内容.
+事实上,基本所有的网站都可以用这个工具来提取代码.
 """
 regex = r"([0-9])|(>(.*?)<)"
 text=""
@@ -29,10 +30,13 @@ def re_html_label(match_h):
         return '>'
     elif match_h.group('and')=='&amp;':
         return '&'
+    elif match_h.group('space')=='&nbsp;':
+        return ' '
     else:
         pass
     
-regex2 = r"(?P<left_angle_bracket>&lt;)|(?P<right_angle_bracket>&gt;)|(?P<and>&amp;)"
+regex2 = r"(?P<left_angle_bracket>&lt;)|(?P<right_angle_bracket>&gt;)|\
+        (?P<and>&amp;)|(?P<space>&nbsp;)"
 text=re.sub(regex2,re_html_label,text)
 
 print(text)
